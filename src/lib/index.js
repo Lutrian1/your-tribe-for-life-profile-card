1da4aec2a2611e668e1cc0d42065368dac819f4d
Luuk <147055722+Lutrian1@users.noreply.github.com>
2025-09-18 22:23:09 +0200

    #4: Could not resolve deploy error - FIX

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,16 +1,17 @@
 // place files you want to import through the `$lib` alias in this folder.
 
 // Components
-export { default as Logo } from './components/Logo.svelte'
-export { default as Header } from './components/Header.svelte'
-export { default as Background } from './components/Background.svelte'
-export { default as Main_content } from './components/Main_content.svelte'
-export { default as Sub_content} from './components/Sub_content.svelte'
-export { default as Magnifying_glass} from './components/Magnifying_glass.svelte'
-export { default as Tape} from './components/Tape.svelte'
-export { default as Play} from './components/Play.svelte'
 
-export { default as Line} from './assets/Line.svelte'
-export { default as GithubLogo} from './assets/GithubLogo.svelte'
-export { default as InstagramSVG} from './assets/InstagramSVG.svelte'
-export { default as BlueskySVG} from './assets/BlueskySVG.svelte'
+export { default as Logo } from '$lib/components/Logo.svelte'
+export { default as Header } from '$lib/components/Header.svelte'
+export { default as Background } from '$lib/components/Background.svelte'
+export { default as Main_content } from '$lib/components/Main_content.svelte'
+export { default as Sub_content} from '$lib/components/Sub_content.svelte'
+export { default as Magnifying_glass} from '$lib/components/Magnifying_glass.svelte'
+export { default as Tape} from '$lib/components/Tape.svelte'
+export { default as Play} from '$lib/components/Play.svelte'
+
+export { default as Line} from '$lib/assets/Line.svelte'
+export { default as GithubLogo} from '$lib/assets/GithubLogo.svelte'
+export { default as InstagramSVG} from '$lib/assets/InstagramSVG.svelte'
+export { default as BlueskySVG} from '$lib/assets/BlueskySVG.svelte'
